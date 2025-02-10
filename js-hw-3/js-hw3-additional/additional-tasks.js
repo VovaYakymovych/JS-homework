f86b913004336ba19a760fd5be7c44580da33a3b
VovaYakymovych <yakym.volodya@gmail.com>
2025-02-10 03:35:11 +0200

    hw 3 upd 3

---
+++ js-hw-3/js-hw3-additional/additional-tasks.js
@@ -0,0 +1,123 @@
+// ================================task-#WpkK0ZH1====================================
+
+// console.log('=====string=======')
+// let str = ['abc', 'qwe', 'asd', 'tyu', 'zxc']
+// console.log(str)
+//
+// console.log('=====numbers=======')
+// let nmb = [1, 2, 3, 4, 5]
+// console.log(nmb)
+//
+// console.log('=====boolean=======')
+// let bool = [true, false, true, false, true]
+// console.log(bool)
+//
+// let list = [str, nmb, bool]
+// console.log(list)
+
+// ================================task-#4aDbSgh====================================
+
+// let list =[];
+// list[0]='abc';
+// list[1]='qwe';
+// list[2]=1;
+// list[3]=123;
+// list[4]=true
+//
+// console.log(list)
+
+
+// ================================task-#qLQLJSeN7i====================================
+
+let list = [2,17,13,6,22,31,45,66,100,-18];
+
+// //1.
+// console.log('========1=======');
+// let i = 0;
+// while(i<list.length){
+//     let item = list[i]
+//     console.log(item)
+//     i++
+// }
+
+// //2.
+// console.log('========2=======');
+//
+// for (let i=0; i<list.length; i++) {
+//     let item = list[i]
+//     console.log(item)
+// }
+
+// //3.
+// console.log('========3=======');
+//
+// let i = 1;
+// while(i<list.length){
+//     let item = list[i]
+//     console.log(i, item)
+//     i +=2
+// }
+
+// //4.
+// console.log('========4=======');
+//
+// for (let i=1; i<list.length; i+=2) {
+//     let item = list[i]
+//     console.log(i, item)
+// }
+
+// //5.
+// console.log('========5=======');
+//
+// let i = 0;
+// while(i<list.length){
+//     let item = list[i]
+//     console.log(i, item)
+//     i +=2
+// }
+
+// //6.
+// console.log('========6=======');
+//
+// for (let i=0; i<list.length; i+=2) {
+//     let item = list[i]
+//     console.log(i, item)
+// }
+
+// //7.
+// console.log('========7=======');
+//
+// for (let i=0; i<list.length; i++) {
+//     let item = list[i]
+//     if (item % 3 === 0){
+//         item = 'okten'
+//     }
+//     console.log(i,item)
+// }
+
+// //8.
+// console.log('========8=======');
+//
+// list.reverse()
+// console.log(list)
+
+//9.
+console.log('========9.=======');
+let i = list.length-1;
+while(i>=0){
+    let item = list[i]
+    console.log(item)
+    i--
+}
+console.log('======')
+for (let i=list.length-1; i>=0; i--) {
+    let item = list[i]
+    console.log(item)
+}
+
+console.log('=====')
+while(i<list.length){
+    let item = list[i]
+    console.log(i, item)
+    i +=2
+}
