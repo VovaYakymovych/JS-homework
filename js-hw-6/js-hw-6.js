7a108a83af2aacf4858ba4b99d01a37136c1700a
VovaYakymovych <yakym.volodya@gmail.com>
2025-02-18 00:24:04 +0200

    homework 6

---
+++ js-hw-6/js-hw-6.js
@@ -0,0 +1,111 @@
+// ==================================task #dFeorS3m7u======================================
+
+
+// let stringLength =(items) =>{
+//   for (let listElement of items) {
+//     console.log('element №',items.indexOf(listElement)+1,'length =', listElement.length)
+//   }
+// }
+//
+// let listStr = ['hello world', 'lorem ipsum', 'javascript is cool']
+// stringLength(listStr)
+
+
+// ==================================task #8lld9HMxXWB======================================
+
+
+// let stringToUp = (items) =>{
+//   for (let item of items) {
+//     console.log('element №',items.indexOf(item)+1, '=', item.toUpperCase())
+//   }
+// }
+//
+// let listStr = ['hello world', 'lorem ipsum', 'javascript is cool']
+// stringToUp(listStr)
+
+
+// ==================================task #ClDsAm7xba7======================================
+
+
+// let stringToLow = (items) =>{
+//   for (let item of items) {
+//     console.log('element №',items.indexOf(item)+1, '=', item.toLowerCase())
+//   }
+// }
+//
+// let listStr = ['HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL']
+// stringToLow(listStr)
+
+
+// ==================================task #0b89BkYZwu======================================
+
+
+// let str = ' dirty string   '
+// console.log(str.trim());
+// знайшов в інтернеті таку функцію
+
+
+// ==================================task #bfoJuse4ZzP======================================
+
+
+// let stringToarray =(str)=>{
+//   console.log(str.split(' '));
+// }
+//
+// let str = 'Ревуть воли як ясла повні';
+// stringToarray(str);
+
+
+// ==================================task #Rbr5kEQ======================================
+
+
+// let numbers = [10,8,-7,55,987,-1011,0,1050,0]
+// console.log(numbers.map(num => `${num}`));
+
+
+// ==================================task #5hqyKTfmc======================================
+
+
+// let sortNums = (array, direction) => {
+//   if (direction === 'ascending') {
+//     console.log(array.sort((a, b) => a - b));
+//   }
+//   else if (direction === 'descending') {
+//     console.log(array.sort((a, b) => b - a));
+//   }
+// }
+//
+// let nums = [11, 21, 3];
+// sortNums(nums, 'ascending')
+// sortNums(nums, 'descending')
+
+
+// ==================================task #yo06d74c1C======================================
+
+
+// let coursesAndDurationArray = [
+//   {title: 'JavaScript Complex', monthDuration: 5},
+//   {title: 'Java Complex', monthDuration: 6},
+//   {title: 'Python Complex', monthDuration: 6},
+//   {title: 'QA Complex', monthDuration: 4},
+//   {title: 'FullStack', monthDuration: 7},
+//   {title: 'Frontend', monthDuration: 4}
+// ];
+//
+// let sortCourses = coursesAndDurationArray.sort((a,b)=>{
+//   return b.monthDuration - a.monthDuration
+// })
+// console.log(sortCourses)
+//
+// let filterCourses = coursesAndDurationArray.filter(value => value.monthDuration>5)
+// console.log(filterCourses)
+//
+// let mappedCourses = coursesAndDurationArray.map((value, index) => {
+//   return {id: index + 1, tittle: value.tittle, monthDuration: value.monthDuration}
+// })
+// console.log(mappedCourses)
+
+
+// ==================================task #bolvdlhP======================================
+
+
