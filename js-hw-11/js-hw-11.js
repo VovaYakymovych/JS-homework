ae04efc6bcb5aae7a6c85c78ad7136e93e83c1dd
VovaYakymovych <yakym.volodya@gmail.com>
2025-03-09 16:35:50 +0200

    hw 11 upd

--- js-hw-11/js-hw-11.js
+++ js-hw-11/js-hw-11.js
@@ -1,19 +1,53 @@
 // ==================================task #HmvAfRQM======================================
 
-fetch('https://dummyjson.com/products')
-  .then(value => value.json())
-  .then(res => {
-    console.log(res);
-    let {products} = res;
+fetch('https://dummyjson.com/carts')
+    .then(response => response.json())
+    .then(res => {
+        const carts = res.carts;
+        console.log(carts);
 
-    document.write(`<div>`);
-    for (const product of products) {
-      document.write(`<div>`);
-      document.write(`<p>${product.id}___ ${product.brand} - ${product.price}</p>`);
-      document.write(`<img src="${product.thumbnail}" alt="">`)
-      document.write(`</div>`);
-    }
-    document.write(`</div>`);
+        let wrapper = document.createElement('div')
+        wrapper.classList.add('wrapper')
+
+        for (let cart of carts) {
+
+            let cartWrapper = document.createElement('div')
+            cartWrapper.classList.add('cartWrapper')
+            cartWrapper.innerHTML = `
+
+                <div class = "cartHeader">
+                    <h2>Кошик №: ${cart.id}</h2>
+                    <p><strong>Користувач ID:</strong> ${cart.userId}</p>
+                    <p><strong>Кількість товарів:</strong> ${cart.totalProducts}</p>
+                    <p><strong>Загальна сума:</strong> $${cart.total.toFixed(2)}</p>
+                    <p><strong>Сума зі знижкою:</strong> $${cart.discountedTotal.toFixed(2)}</p>
+                    <h3>Товари:</h3>
+                <div>
+            `
+            let products = document.createElement('div')
+            products.classList.add('products')
+
+            for (const product of cart.products) {
+                let productWrapper = document.createElement('div')
+                productWrapper.classList.add('productWrapper')
+                productWrapper.innerHTML = `
+                
+                <img src="${product.thumbnail}" alt="${product.title}">
+                    <div>
+                        <h4>${product.title}</h4>
+                        <p>Ціна: $${product.price}</p>
+                        <p>Кількість: ${product.quantity}</p>
+                        <p>Загальна сума: $${product.total.toFixed(2)}</p>
+                        <p>Зі знижкою: $${product.discountedTotal.toFixed(2)}</p>
+                    </div>
+                
+                `
+                products.appendChild(productWrapper)
+            }
+            cartWrapper.appendChild(products)
+            wrapper.appendChild(cartWrapper)
+        }
+        document.body.appendChild(wrapper)
+    })
 
 
-  });
